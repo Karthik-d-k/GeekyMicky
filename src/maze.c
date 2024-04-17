b838d9dff060d6c9d6268f548f3062addf991980
Karthik D K <karthikdk1998@gmail.com>
2024-04-17 08:44:11 +0530

    small refactoring

--- src/maze.c
+++ src/maze.c
@@ -4,6 +4,96 @@
 
 #include "maze.h"
 
+uint8_t COST[MAZE_SIZE][MAZE_SIZE];
+WallInfo WALLS[MAZE_SIZE][MAZE_SIZE];
+
+void initialise_walls() {
+    for (int r = 0; r < MAZE_SIZE; r++) {
+        for (int c = 0; c < MAZE_SIZE; c++) {
+            WALLS[r][c].north = UNKNOWN;
+            WALLS[r][c].east = UNKNOWN;
+            WALLS[r][c].south = UNKNOWN;
+            WALLS[r][c].west = UNKNOWN;
+        }
+    }
+    for (int r = 0; r < MAZE_SIZE; r++) {
+        WALLS[r][0].south = WALL;
+        WALLS[r][MAZE_SIZE - 1].north = WALL;
+    }
+    for (int c = 0; c < MAZE_SIZE; c++) {
+        WALLS[0][c].west = WALL;
+        WALLS[MAZE_SIZE - 1][c].east = WALL;
+    }
+    // set_wall_state(START, EAST, WALL);
+    WALLS[0][0].east = WALL;
+    // set_wall_state(START, NORTH, EXIT);
+    WALLS[0][0].north = EXIT;
+}
+
+bool is_cell_accessible(CELL cell, int direction) {
+    bool result = false;
+    WallInfo walls = WALLS[cell.r][cell.c];
+    switch (direction) {
+    case 0:
+        result = (walls.north & MASK_OPEN) == EXIT;
+        break;
+    case 1:
+        result = (walls.east & MASK_OPEN) == EXIT;
+        break;
+    case 2:
+        result = (walls.south & MASK_OPEN) == EXIT;
+        break;
+    case 3:
+        result = (walls.west & MASK_OPEN) == EXIT;
+        break;
+    default:
+        result = false;
+        break;
+    }
+
+    return result;
+}
+
+CELL neighbour_cell(CELL cell, int direction) {
+    switch (direction) {
+    case 0: { // North
+        uint8_t new_y = cell.c + 1;
+        if (new_y >= MAZE_SIZE) {
+            return cell;
+        }
+        CELL neighbour_cell = {cell.r, new_y};
+        return neighbour_cell;
+    }
+    case 1: { // East
+        uint8_t new_x = cell.r + 1;
+        if (new_x >= MAZE_SIZE) {
+            return cell;
+        }
+        CELL neighbour_cell = {new_x, cell.c};
+        return neighbour_cell;
+    }
+    case 2: { // South
+        uint8_t new_y = cell.c - 1;
+        if (new_y < 0) {
+            return cell;
+        }
+        CELL neighbour_cell = {cell.r, new_y};
+        return neighbour_cell;
+    }
+    case 3: { // West
+        uint8_t new_x = cell.r - 1;
+        if (new_x < 0) {
+            return cell;
+        }
+        CELL neighbour_cell = {new_x, cell.c};
+        return neighbour_cell;
+    }
+    default:
+        // Invalid direction, return current cell
+        return cell;
+    }
+}
+
 void print_maze(uint8_t cost[MAZE_SIZE][MAZE_SIZE]) {
     // Print the top border of the maze
     for (int col = MAZE_SIZE - 1; col >= 0; col--) {
--- src/maze.h
+++ src/maze.h
@@ -8,6 +8,9 @@
 #define MAZE_CELL_COUNT (MAZE_SIZE * MAZE_SIZE)
 #define MAX_COST (MAZE_CELL_COUNT - 1)
 
+#define START \
+    CELL { 0, 0 }
+
 typedef struct {
     uint8_t r;
     uint8_t c;
@@ -19,6 +22,33 @@ typedef enum { NORTH,
                WEST,
                HEADING_COUNT,
                BLOCKED = 99 } DIRECTION;
+
+typedef enum {
+    EXIT = 0b00,    // a wall that has been seen and confirmed absent
+    WALL = 0b01,    // a wall that has been seen and confirmed present
+    UNKNOWN = 0b10, // a wall that has not yet been seen
+    VIRTUAL = 0b11, // a wall that has not yet been seen
+} WallState;
+
+typedef struct {
+    WallState north : 2;
+    WallState east : 2;
+    WallState south : 2;
+    WallState west : 2;
+} WallInfo;
+
+typedef enum {
+    MASK_OPEN = 0x01,   // open maze for search
+    MASK_CLOSED = 0x03, // closed maze for fast run
+} MazeMask;
+
+void initialise_walls();
+
+void set_wall_state(CELL loc, DIRECTION heading, WallState state);
+
+bool is_cell_accessible(CELL cell, int direction);
+
+CELL neighbour_cell(CELL cell, int direction);
 
 void print_maze(uint8_t cost[MAZE_SIZE][MAZE_SIZE]);
 

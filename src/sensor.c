c6250130b88eb3fc4339bc206e79824a6d1b8e85
Karthik D K <karthikdk1998@gmail.com>
2024-04-28 14:18:29 +0530

    add ir sensor calcs

---
+++ src/sensor.c
@@ -0,0 +1,28 @@
+#include "sensor.h"
+
+IRInfo get_ir_sensor_values(uint8_t pin) {
+    // Read analog value from sensor
+    uint16_t adc_value = analogRead(pin);
+
+    // Constrain sensor values to remain within set min-max range
+    adc_value = constrain(adc_value, 0, 1023);
+
+    // Calculate distance from power fit function
+    uint8_t distance = (uint8_t)(POWER_COEFF_C * pow(adc_value, POWER_COEFF_P));
+
+    IRInfo ir_info = {adc_value, distance};
+    return ir_info;
+}
+
+// -------------------TEST / USAGE ------------------------- //
+int test_ir_sensors(void) {
+    IRInfo front_ir = get_ir_sensor_values(FRONT_IR_PIN);
+    IRInfo right_ir = get_ir_sensor_values(RIGHT_IR_PIN);
+    IRInfo left_ir = get_ir_sensor_values(LEFT_IR_PIN);
+
+    printf("[FRONT_IR] adc_value: [%d] \t distance : [%d] \n", front_ir.adc_value, front_ir.distance);
+    printf("[RIGHT_IR] adc_value: [%d] \t distance : [%d] \n", right_ir.adc_value, right_ir.distance);
+    printf("[LEFT_IR ] adc_value: [%d] \t distance : [%d] \n", left_ir.adc_value, left_ir.distance);
+
+    return 0;
+}
---
+++ src/sensor.h
@@ -0,0 +1,21 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include <Arduino.h>
+
+// Power coefficients and Pins for Sharp IR sensor
+#define FRONT_IR_PIN A0
+#define RIGHT_IR_PIN A1
+#define LEFT_IR_PIN A2
+#define POWER_COEFF_C 116483.399834958
+#define POWER_COEFF_P -1.132373887917551
+
+typedef struct {
+    uint16_t adc_value;
+    uint8_t distance;
+}IRInfo;
+
+// ref: https://github.com/DrGFreeman/SharpDistSensor
+IRInfo get_ir_sensor_values(uint8_t pin);
+
+#endif

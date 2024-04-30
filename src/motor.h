9ae7d208075af5eaba78838ea355795e8239a113
Karthik D K <karthikdk1998@gmail.com>
2024-04-30 10:45:42 +0530

    change pwm pin numbers

--- src/motor.h
+++ src/motor.h
@@ -3,9 +3,9 @@
 
 #include <Arduino.h>
 
-// Motor pins
-#define RIGHT_MOTOR_PWM_PIN A6
-#define LEFT_MOTOR_PWM_PIN A7
+// Motor pins (PWM pins are denoted by `~` sign on pinout diagram)
+#define RIGHT_MOTOR_PWM_PIN D11
+#define LEFT_MOTOR_PWM_PIN D10
 
 void init_motors(void);
 void test_motors(uint8_t motor_pin);

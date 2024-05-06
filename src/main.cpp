ef7f197e5ecc042f034f37c2c05ec076edd19994
Karthik D K <karthikdk1998@gmail.com>
2024-05-06 17:23:07 +0530

    add systicks for timed robot control

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,16 @@
 #include "motor.h"
 #include "queue.h"
 #include "sensor.h"
+#include "systick.h"
 
 extern CELL CURRENT_CELL;
 extern ABSOLUTE_DIRECTION CURRENT_ABSOLUTE_DIRECTION;
 extern uint8_t COST[MAZE_SIZE][MAZE_SIZE];
 extern WallInfo WALLS[MAZE_SIZE][MAZE_SIZE];
+
+ISR(TIMER2_COMPA_vect, ISR_NOBLOCK) {
+    systick_update();
+}
 
 void setup() {
     Serial.begin(BAUDRATE);
@@ -19,6 +24,8 @@ void setup() {
     init_maze();
     init_motors();
     init_ultrasonic();
+    /// do not begin systick until the hardware is setup
+    systick_begin();
 }
 
 void loop() {

2d7ac8e909f4c545e4a7c142ca7dbe9161de05f4
Karthik D K <karthikdk1998@gmail.com>
2024-05-14 16:26:17 +0530

    motors api

--- src/motor.cpp
+++ src/motor.cpp
@@ -12,47 +12,75 @@ void set_pwm_frequency(void) {
     bitSet(TCCR1B, CS10);
 }
 
-void motor_init(MOTOR* motor_ptr, int pin_A, int pin_B) {
-    pinMode(pin_A, OUTPUT);
-    pinMode(pin_B, OUTPUT);
-    motor_ptr->pin_A = pin_A;
-    motor_ptr->pin_B = pin_B;
-    motor_ptr->speed = 0;
-}
-
-void motor_set_forward(MOTOR* motor_ptr, int speed) {
-    fast_write_pin(motor_ptr->pin_A, LOW);
-    analogWrite(motor_ptr->pin_B, speed);
-    motor_ptr->speed = speed;
-}
-
-void motor_set_backward(MOTOR* motor_ptr, int speed) {
-    fast_write_pin(motor_ptr->pin_A, HIGH);
-    analogWrite(motor_ptr->pin_B, speed);
-    motor_ptr->speed = speed;
-}
-
-int motor_get_speed(MOTOR* motor_ptr) {
-    return motor_ptr->speed;
-}
-
 void init_motors(void) {
-    motor_init(&RIGHT_MOTOR, RIGHT_MOTOR_PIN1, RIGHT_MOTOR_PIN2);
-    motor_init(&LEFT_MOTOR, LEFT_MOTOR_PIN1, LEFT_MOTOR_PIN2);
     set_pwm_frequency();
+    pinMode(RIGHT_MOTOR_PIN1, OUTPUT);
+    pinMode(RIGHT_MOTOR_PIN2, OUTPUT);
+    pinMode(LEFT_MOTOR_PIN1, OUTPUT);
+    pinMode(LEFT_MOTOR_PIN2, OUTPUT);
+    pinMode(RIGHT_MOTOR_PWM, OUTPUT);
+    pinMode(LEFT_MOTOR_PWM, OUTPUT);
+    pinMode(MOTOR_STANDBY, OUTPUT);
+
+    fast_write_pin(MOTOR_STANDBY, HIGH);
     stop_motors();
 }
 
 void stop_motors(void) {
-    motor_set_forward(&RIGHT_MOTOR, 0);
-    motor_set_forward(&LEFT_MOTOR, 0);
+    // IN1 IN2 PWM STBY
+    //  L   L   H   H    STOP
+    fast_write_pin(RIGHT_MOTOR_PIN1, LOW);
+    fast_write_pin(RIGHT_MOTOR_PIN2, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN1, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN2, LOW);
+
+    fast_write_pin(RIGHT_MOTOR_PWM, HIGH);
+    fast_write_pin(LEFT_MOTOR_PWM, HIGH);
+}
+
+void forward_motors(int speed) {
+    // IN1 IN2 PWM STBY
+    //  H   L   H   H    CW
+    fast_write_pin(RIGHT_MOTOR_PIN1, HIGH);
+    fast_write_pin(RIGHT_MOTOR_PIN2, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN1, HIGH);
+    fast_write_pin(LEFT_MOTOR_PIN2, LOW);
+
+    analogWrite(RIGHT_MOTOR_PWM, speed);
+    analogWrite(LEFT_MOTOR_PWM, speed);
+}
+
+void backward_motors(int speed) {
+    // IN1 IN2 PWM STBY
+    //  L   H   H   H    STOP
+    fast_write_pin(RIGHT_MOTOR_PIN1, LOW);
+    fast_write_pin(RIGHT_MOTOR_PIN2, HIGH);
+    fast_write_pin(LEFT_MOTOR_PIN1, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN2, HIGH);
+
+    analogWrite(RIGHT_MOTOR_PWM, speed);
+    analogWrite(LEFT_MOTOR_PWM, speed);
+}
+
+void shortbrake_motors(void) {
+    // IN1 IN2 PWM STBY
+    //  H   L   L   H    STOP
+    fast_write_pin(RIGHT_MOTOR_PIN1, HIGH);
+    fast_write_pin(RIGHT_MOTOR_PIN2, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN1, HIGH);
+    fast_write_pin(LEFT_MOTOR_PIN2, LOW);
+
+    fast_write_pin(RIGHT_MOTOR_PWM, LOW);
+    fast_write_pin(LEFT_MOTOR_PWM, LOW);
 }
 
 void test_motors(int speed) {
-    motor_set_forward(&RIGHT_MOTOR, speed);
-    motor_set_forward(&LEFT_MOTOR, speed);
-    delay(2000);
-    motor_set_backward(&RIGHT_MOTOR, speed);
-    motor_set_backward(&LEFT_MOTOR, speed);
-    delay(2000);
+    forward_motors(speed);
+    delay(5000);
+    backward_motors(speed);
+    delay(5000);
+    shortbrake_motors();
+    forward_motors(speed);
+    delay(5000);
+    stop_motors();
 }
--- src/motor.h
+++ src/motor.h
@@ -3,15 +3,17 @@
 
 #include <Arduino.h>
 
-// ref: https://github.com/tylersweat/arduino-MOTOR
-
 // Motor pins (PWM pins are denoted by `~` sign on pinout diagram)
 // Analog pins are referred w/ `Ax` but digital pins are referred w/ numbers
-const uint8_t RIGHT_MOTOR_PIN1 = 11;
-const uint8_t RIGHT_MOTOR_PIN2 = 10;
+const uint8_t RIGHT_MOTOR_PIN1 = 2;
+const uint8_t RIGHT_MOTOR_PIN2 = 3;
+const uint8_t RIGHT_MOTOR_PWM = 10;
 
-const uint8_t LEFT_MOTOR_PIN1 = 6;
+const uint8_t LEFT_MOTOR_PIN1 = 4;
 const uint8_t LEFT_MOTOR_PIN2 = 5;
+const uint8_t LEFT_MOTOR_PWM = 11;
+
+const uint8_t MOTOR_STANDBY = 7;
 
 typedef struct {
     int pin_A;
@@ -21,17 +23,15 @@ typedef struct {
 
 void set_pwm_frequency(void);
 
-void motor_init(MOTOR* motor_ptr, int pin_A, int pin_B);
-
-void motor_set_forward(MOTOR* motor_ptr, int speed);
-
-void motor_set_backward(MOTOR* motor_ptr, int speed);
-
-int motor_get_speed(MOTOR* motor_ptr);
-
 void init_motors(void);
 
 void stop_motors(void);
+
+void forward_motors(int speed);
+
+void backward_motors(int speed);
+
+void shortbrake_motors(void);
 
 void test_motors(int speed);
 

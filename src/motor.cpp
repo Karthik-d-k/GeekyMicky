2d19d324de79ab297dbb36746b8ddda92ec79dbd
Karthik D K <karthikdk1998@gmail.com>
2024-05-17 12:25:50 +0530

    add L9110 and TB6612FNG drivers compatibilty for motor drivers

--- src/motor.cpp
+++ src/motor.cpp
@@ -3,15 +3,13 @@
 #include "config.h"
 #include "motor.h"
 
-MOTOR RIGHT_MOTOR;
-MOTOR LEFT_MOTOR;
-
 void set_pwm_frequency(void) {
     // Divide by 1. frequency = 31.25 kHz;
     bitClear(TCCR1B, CS11);
     bitSet(TCCR1B, CS10);
 }
 
+#if TB6612FNG_ENABLE
 void init_motors(void) {
     set_pwm_frequency();
     pinMode(RIGHT_MOTOR_PIN1, OUTPUT);
@@ -101,6 +99,77 @@ void shortbrake_motors(void) {
     fast_write_pin(RIGHT_MOTOR_PWM, LOW);
     fast_write_pin(LEFT_MOTOR_PWM, LOW);
 }
+
+#elif L9110_ENABLE
+void init_motors(void) {
+    set_pwm_frequency();
+    pinMode(RIGHT_MOTOR_PIN1, OUTPUT);
+    pinMode(LEFT_MOTOR_PIN1, OUTPUT);
+    pinMode(RIGHT_MOTOR_PIN2, OUTPUT);
+    pinMode(LEFT_MOTOR_PIN2, OUTPUT);
+    stop_motors();
+}
+
+void stop_motors(void) {
+    // IN1 IN2 OU1 OU2
+    //  L   L   L   L    STOP
+    fast_write_pin(RIGHT_MOTOR_PIN1, LOW);
+    fast_write_pin(RIGHT_MOTOR_PIN2, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN1, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN2, LOW);
+}
+
+void forward_motors(int speed) {
+    // IN1 IN2 OU1 OU2
+    //  H   L   H   L    CW
+    fast_write_pin(RIGHT_MOTOR_PIN1, speed);
+    fast_write_pin(RIGHT_MOTOR_PIN2, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN1, speed);
+    fast_write_pin(LEFT_MOTOR_PIN2, LOW);
+}
+
+void backward_motors(int speed) {
+    // IN1 IN2 OU1 OU2
+    //  L   H   L   H    CCW
+    fast_write_pin(RIGHT_MOTOR_PIN1, LOW);
+    fast_write_pin(RIGHT_MOTOR_PIN2, speed);
+    fast_write_pin(LEFT_MOTOR_PIN1, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN2, speed);
+}
+
+void right_motors(int speed) {
+    // IN1 IN2 OU1 OU2
+    //  L   H   L   H    CCW (RIGHT)
+    // IN1 IN2 OU1 OU2
+    //  H   L   H   L    CW (LEFT)
+    fast_write_pin(RIGHT_MOTOR_PIN1, LOW);
+    fast_write_pin(RIGHT_MOTOR_PIN2, speed);
+    fast_write_pin(LEFT_MOTOR_PIN1, speed);
+    fast_write_pin(LEFT_MOTOR_PIN2, LOW);
+}
+
+void left_motors(int speed) {
+    // IN1 IN2 OU1 OU2
+    //  H   L   H   L    CW (RIGHT)
+    // IN1 IN2 OU1 OU2
+    //  L   H   L   H    CCW (LEFT)
+    fast_write_pin(RIGHT_MOTOR_PIN1, speed);
+    fast_write_pin(RIGHT_MOTOR_PIN2, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN1, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN2, speed);
+}
+
+void shortbrake_motors(void) {
+    // short brake is not available in L9110, use stop instead
+    // IN1 IN2 OU1 OU2
+    //  L   L   L   L    STOP
+    fast_write_pin(RIGHT_MOTOR_PIN1, LOW);
+    fast_write_pin(RIGHT_MOTOR_PIN2, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN1, LOW);
+    fast_write_pin(LEFT_MOTOR_PIN2, LOW);
+}
+
+#endif
 
 void test_motors(int speed) {
     forward_motors(speed);
--- src/motor.h
+++ src/motor.h
@@ -3,8 +3,13 @@
 
 #include <Arduino.h>
 
+#define TB6612FNG_ENABLE false
+#define L9110_ENABLE !TB6612FNG_ENABLE
+
 // Motor pins (PWM pins are denoted by `~` sign on pinout diagram)
 // Analog pins are referred w/ `Ax` but digital pins are referred w/ numbers
+
+#if TB6612FNG_ENABLE
 const uint8_t RIGHT_MOTOR_PIN1 = 2;
 const uint8_t RIGHT_MOTOR_PIN2 = 3;
 const uint8_t RIGHT_MOTOR_PWM = 10;
@@ -14,14 +19,16 @@ const uint8_t LEFT_MOTOR_PIN2 = 5;
 const uint8_t LEFT_MOTOR_PWM = 11;
 
 const uint8_t MOTOR_STANDBY = 7;
+#elif L9110_ENABLE
+const uint8_t RIGHT_MOTOR_PIN1 = 3;
+const uint8_t RIGHT_MOTOR_PIN2 = 10;
+
+const uint8_t LEFT_MOTOR_PIN1 = 5;
+const uint8_t LEFT_MOTOR_PIN2 = 11;
+#endif
+
 const int FORWARD_MOTOR_SPEED = 100;
 const int TURNING_MOTOR_SPEED = 100;
-
-typedef struct {
-    int pin_A;
-    int pin_B;
-    int speed;
-} MOTOR;
 
 void set_pwm_frequency(void);
 

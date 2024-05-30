4bfb3ac359bb60b354fe6398628f1178a3df4097
Karthik D K <karthikdk1998@gmail.com>
2024-05-30 07:09:06 +0530

    remove enable pin

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 #include "sensor.h"
 #include "systick.h"
 
-const uint8_t ENABLE_PIN = 6; // Digital pin to enable the control loop
 volatile bool robot_enabled = false;
 
 extern CELL CURRENT_CELL;
@@ -24,31 +23,21 @@ ISR(TIMER2_COMPA_vect, ISR_NOBLOCK) {
 void setup() {
     Serial.begin(BAUDRATE);
     Serial.println("GeekyMicky");
-    pinMode(ENABLE_PIN, INPUT);
+    Serial.println("----------");
 
     init_maze();
     init_motors();
     init_ultrasonic();
+    delay(500);
     /// do not begin systick until the hardware is setup
     systick_begin();
 }
 
-void loop() { // Check the state of the control loop enable pin
-    if (digitalRead(ENABLE_PIN) == HIGH) {
-        robot_enabled = true;
-    } else {
-        robot_enabled = false;
-    }
+void loop() {
+    Serial.println("Running...\n");
 
-    // Execute the control loop tasks if enabled
-    if (robot_enabled) {
-        Serial.println("Running...\n");
+    CELL target = END;
+    search_to(target);
 
-        CELL target = END;
-        search_to(target);
-
-        Serial.println("SUCCESS !!");
-    } else {
-        // Put the Arduino in idle mode when the control loop is disabled
-    }
+    Serial.println("SUCCESS !!");
 }
